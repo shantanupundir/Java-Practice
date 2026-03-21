8bb3aee2dd894b4b4cb42b639d40167bf23e37f5
shantanupundir <ranaaryan997@gmail.com>
2026-03-21 14:33:10 +0530

    hollow pyramid star pattern code commit

---
+++ hollowpyramid.java
@@ -0,0 +1,19 @@
+public class hollowpyramid {
+  public static void main(String[] args) {
+    int n=5;
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=2*i-1;j++){
+        if(i==n || j==1 || j==2*i-1){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
