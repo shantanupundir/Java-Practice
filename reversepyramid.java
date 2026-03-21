c672015461cfaeba2798e851357d329db6ace260
shantanupundir <ranaaryan997@gmail.com>
2026-03-21 14:48:46 +0530

    reverse pyramid pattern code commit

---
+++ reversepyramid.java
@@ -0,0 +1,16 @@
+public class reversepyramid {
+  public static void main(String[] args) {
+    for(int i=0;i<5;i++){
+      for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      for(int j=0;j<2*(5-i)-1;j++){
+        System.out.print("*");
+      }
+      for(int j=0;j<i;j++){
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+}
