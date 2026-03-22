b01bb00fbd199d8b0f5c114e0843f784c44acec4
shantanupundir <ranaaryan997@gmail.com>
2026-03-22 20:36:09 +0530

    smallest element in the array code commit

---
+++ smallestElement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class smallestElement {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array:");
+    int size = sc.nextInt();
+    System.out.println("Enter the elements of the array:");
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++) {
+      arr[i] = sc.nextInt();
+    }
+    int smallest = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] < smallest) {
+        smallest = arr[i];
+      }
+    }
+    System.out.println("Smallest element in the array: " + smallest);
+  }
+}
