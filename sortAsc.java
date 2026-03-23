57a0aae2d8febbe8f90e59469a15b5c95a92818d
shantanupundir <ranaaryan997@gmail.com>
2026-03-23 12:48:25 +0530

    sort the array in ascending order code commit

---
+++ sortAsc.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class sortAsc {
+  public static void main(String[] args) {
+    int arr[] = {5, 2, 8, 1, 3};
+    // Arrays.sort(arr);
+    // System.out.println("Sorted array in ascending order: " + Arrays.toString(arr));
+    for(int i = 0; i < arr.length - 1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
+
+                if(arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        // print array
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+  }
+}
