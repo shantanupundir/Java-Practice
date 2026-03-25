9e61d4c8f4e2ca562d8e57ac364531adb56d0356
shantanupundir <ranaaryan997@gmail.com>
2026-03-25 10:46:21 +0530

    count words in a sentence code commit

---
+++ countWords.java
@@ -0,0 +1,7 @@
+public class countWords {
+  public static void main(String[] args) {
+    String str = "Hello world this is a test";
+    String words[] = str.split(" ");
+    System.out.println("Number of words: " + words.length);
+  }
+}
