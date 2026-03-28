803728f45d22876e7d3c0a7a608be001c83b0286
shantanupundir <ranaaryan997@gmail.com>
2026-03-28 20:40:05 +0530

    Prine each charcter of a string in new line code commit

---
+++ nextLine.java
@@ -0,0 +1,8 @@
+public class nextLine {
+  public static void main(String[] args) {
+    String s = "Hello World";
+    for (char ch : s.toCharArray()) {
+      System.out.println(ch);
+    }
+  }
+}
