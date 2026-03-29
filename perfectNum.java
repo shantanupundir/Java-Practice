ec9025657fad718130699230288a1202cb8aa53f
shantanupundir <ranaaryan997@gmail.com>
2026-03-29 12:47:37 +0530

    check number is perfect or not code commit

---
+++ perfectNum.java
@@ -0,0 +1,16 @@
+public class perfectNum {
+  public static void main(String[] args) {
+    int num = 28;
+    int sum = 0;
+    for(int i=1; i<num; i++){
+      if(num%i == 0){
+        sum += i;
+      }
+    }
+    if(sum == num){
+      System.out.println(num + " is a perfect number");
+    }else{
+      System.out.println(num + " is not a perfect number");
+    }
+  }
+}
